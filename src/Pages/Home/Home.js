fef913b066ebb4a2d34abb681f08db04ea819366
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-11-09 02:31:20 +0800

    services fake data loaded

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Services from "../Services/Services";
 
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
+      <Services></Services>
+      <Link>
+        <button>Button Services</button>
+      </Link>
     </div>
   );
 };
