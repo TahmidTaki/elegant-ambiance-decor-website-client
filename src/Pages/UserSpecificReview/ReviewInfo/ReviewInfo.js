6f039d3901e80bd2f091e21cb9b5f893c7a19fd5
TahmidTaki <tahmid.taki@live.iium.edu.my>
2022-11-10 14:19:08 +0800

    individual reviews page design implemented

--- src/Pages/UserSpecificReview/ReviewInfo/ReviewInfo.js
+++ src/Pages/UserSpecificReview/ReviewInfo/ReviewInfo.js
@@ -16,24 +16,59 @@ const ReviewInfo = ({ review, handleDelete }) => {
 
   return (
     <div>
-      <button onClick={() => handleDelete(_id)} className="p-4 bg-rose-400">
-        X
-      </button>
-      <Link to={`/editReview/${_id}`}>
-        <button>edit review</button>
-      </Link>
+      {/* prev code starts */}
 
-      <p>{serviceName}</p>
-      <p>{reviewer}</p>
-      <p>{email}</p>
-      <img src={reviewerImg} alt="" className="w-8 h-8" />
-      {serviceDetail?.img && (
-        <p>
-          Srvice img: <img src={serviceDetail.img} alt="" className="w-8 h-8" />
-        </p>
-      )}
-      <p>{message}</p>
-      <hr />
+      {/* prev code ends */}
+      <div className="dark:bg-gray-800 dark:text-gray-100">
+        <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-gray-900">
+          <div className="">
+            <div>
+              <img
+                src={reviewerImg}
+                alt="avatar"
+                className="object-cover w-10 h-10 mx-auto rounded-full bg-gray-500"
+              />
+              <span className="hover:underline dark:text-gray-400">{reviewer}</span>
+            </div>
+            <p>Reviewer Email: {email}</p>
+          </div>
+          <div className="mt-3">
+            <p className="text-2xl font-bold hover:underline">{serviceName}</p>
+            <div>
+              {serviceDetail?.img && (
+                <p>
+                  Service img: <img src={serviceDetail.img} alt="" className="w-64 h-64 mx-auto" />
+                </p>
+              )}
+            </div>
+            <p className="mt-2">{message}</p>
+          </div>
+          <div className="flex justify-center p-2">
+            <Link to={`/editReview/${_id}`} className="hover:underline text-orange-400">
+              <button className="btn btn-warning mx-3">Edit Review</button>
+            </Link>
+            <div className="">
+              <button onClick={() => handleDelete(_id)} className="btn btn-square btn-outline">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr class="my-8 h-px bg-amber-700 border-0 "></hr>
     </div>
   );
 };
